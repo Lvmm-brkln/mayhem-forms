e3ad9cb3096de543bf4f8e295a33680aa6ca602b
Lvmmbrkln <j.degueltzl@gmail.com>
2018-07-31 19:23:19 +0200

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/home', to: 'static_pages#home'
+  get '/', to: 'static_pages#home'
   get '/new', to: 'users_controller#new'
   post '/new', to: 'users_controller#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
